bc66d1d926c9d903c803e5241e95889c5dfe3c86
taeyeon <binnimhi@naver.com>
2026-09-04 17:57:47 +0900

    refactor:: 메인 메뉴 아이템을 개별 컴포넌트로 분리

--- src/widgets/main_menu/ui/MainMenuItemList.tsx
+++ src/widgets/main_menu/ui/MainMenuItemList.tsx
@@ -1,13 +1,16 @@
 import styled from "@emotion/styled";
-import MainMenuItem from "./MainMenuItem";
+import CombineMenuItem from "./eachButton/CombineMenuItem";
+import RandomMenuItem from "./eachButton/RandomMenuItem";
+import InventoryMenuItem from "./eachButton/InventoryMenuItem";
+import SubmitMenuItem from "./eachButton/SubmitMenuItem";
 
 export default function MainMenuItemList() {
   return (
     <Wrapper>
-      <MainMenuItem text="제출" />
-      <MainMenuItem text="조합" />
-      <MainMenuItem text="인벤" />
-      <MainMenuItem text="뽑기" />
+      <SubmitMenuItem />
+      <CombineMenuItem />
+      <InventoryMenuItem />
+      <RandomMenuItem />
     </Wrapper>
   );
 }
@@ -18,32 +21,4 @@ const Wrapper = styled.div`
   margin-top: 16px;
   grid-template-columns: repeat(2, 1fr);
   row-gap: 0;
-
-  & > * {
-    justify-self: center;
-  }
-
-  & > :nth-of-type(1) {
-    justify-self: end;
-    margin-right: 24.5px;
-  }
-
-  & > :nth-of-type(2) {
-    justify-self: start;
-    margin-left: 24.5px;
-  }
-
-  & > :nth-of-type(n + 3) {
-    transform: translateY(-6px);
-  }
-
-  & > :nth-of-type(3) {
-    justify-self: start;
-    margin-left: 5px;
-  }
-
-  & > :nth-of-type(4) {
-    justify-self: end;
-    margin-right: 5px;
-  }
 `;
