e2395db0554c0d2fb5fd6fbc9da5a3188261bc99
taeyeon <binnimhi@naver.com>
2026-09-18 08:20:18 +0900

    feat:: 보유/예약중인 아이템이 없을 경우 '아이템이 없습니다' 예외처리 문구 추가

---
+++ src/features/itemFiltering/ui/EmptyList.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+interface EmptyListProps {
+  text: string;
+}
+
+export default function EmptyList({ text }: EmptyListProps) {
+  return <Text>{text}</Text>;
+}
+
+const Text = styled.p`
+  color: var(--color-gray-600, #868e96);
+  text-align: center;
+  white-space: pre-line;
+
+  /* body/body-medium */
+  font-family: Pretendard;
+  font-size: var(--typo-body-medium, 16px);
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 24px */
+`;
